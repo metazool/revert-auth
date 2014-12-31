c6f73cd0c4ba9bc88bf144a96e24a0a9e08ffb09
Jo Walsh <metazool@fastmail.net>
2014-12-31 09:29:53 +0000

    A straight copy of http://svn.openstreetmap.org/applications/utils/revert/ with intentions for changes added to the README, and an empty test directory.

---
+++ Delete.pm
@@ -0,0 +1,210 @@
+#!/usr/bin/perl
+
+# Delete.pm
+# ---------
+#
+# Deletes an object.
+#
+# Part of the "osmtools" suite of programs
+# Originally written by Frederik Ramm <frederik@remote.org>; public domain
+#
+
+
+package Delete;
+
+use strict;
+use warnings;
+
+use OsmApi;
+
+our $globalListOfDeletedStuff = {};
+
+# deletes one object
+#
+# parameters: 
+#   $what: 'node', 'way', or 'relation'
+#   $id: object id
+#   $changeset: id of changeset to use for delete operation
+# return:
+#   success=1 failure=undef
+
+sub delete
+{
+    my ($what, $id, $changeset) = @_;
+    # this will try to remove not only the object but all its members
+    # e.g. remove a way plus nodes
+    my $recurse = 0;
+    # this will try to modify any object that contains the object-to-be-deleted
+    # by removing the object-to-be-deleted from it
+    my $remove_references = 0;
+    # this will delete all objects referencing the object-to-be-deleted.
+    my $cascade = 0;
+
+    my $xml = determine_delete_action($what, $id, $changeset, $recurse, 0);
+    return undef unless defined ($xml);
+
+    my $modify = "";
+    my $delete_cascade = "";
+    my $loop = 1;
+    while ($loop)
+    {
+        $loop = 0;
+        my $osc = <<EOF;
+<osmChange version='0.6'>
+<modify>
+$modify
+</modify>
+<delete>
+$delete_cascade
+</delete>
+<delete>
+$xml
+</delete>
+</osmChange>
+EOF
+        my $resp = OsmApi::post("changeset/$changeset/upload", $osc);
+        if (!$resp->is_success)
+        {
+            my $c = $resp->content();
+            print "$c\n";
+            if ($c =~ /(\S+) (\d+) (is )?still used by (\S+) ([0-9,]+)/ || $c =~ /The (\S+) (\d+) (is )?used in (\S+) ([0-9,]+)/)
+            {
+                if ($remove_references)
+                {
+                    my ($what2, $id2, $referer, $referer_ids) = (lc($1),$2,$4,$5);
+                    print STDERR "$what2 $id2 still used by $referer $referer_ids; removing it from there\n";
+                    $referer = $1 if ($referer =~ /(.*)s$/);
+                    foreach my $referer_id(split(/,/, $referer_ids))
+                    {
+                        my $obj = OsmApi::get("$referer/$referer_id");
+                        foreach (split(/\n/, $obj->content()))
+                        { 
+                            next if (/<\?xml/);
+                            next if (/<osm/);
+                            next if (/<\/osm/);
+                            next if (/<nd ref="$id2"/) && ($what2 eq "node");
+                            next if (/<member type="$what2" ref="$id2"/);
+                            s/changeset="\d+"/changeset="$changeset"/;
+                            $modify .= $_;
+                        }
+                    }
+                    $loop=1;
+                    print "--$modify--\n";
+                }
+                elsif ($cascade)
+                {
+                    my ($what2, $id2, $referer, $referer_ids) = (lc($1),$2,$4,$5);
+                    print STDERR "$what2 $id2 still used by $referer $referer_ids; removing those\n";
+                    $referer = $1 if ($referer =~ /(.*)s$/);
+                    foreach my $referer_id(split(/,/, $referer_ids))
+                    {
+                        my $obj = OsmApi::get("$referer/$referer_id");
+                        my $del;
+                        foreach (split(/\n/, $obj->content()))
+                        { 
+                            next if (/<\?xml/);
+                            next if (/<osm/);
+                            next if (/<\/osm/);
+                            next if (/<nd/);
+                            next if (/<tag/);
+                            next if (/<member/);
+                            s/changeset="\d+"/changeset="$changeset"/;
+                            $del .= $_;
+                        }
+                        $delete_cascade = $del.$delete_cascade;
+                    }
+                    $loop=1;
+                }
+            }
+            else
+            {
+                print STDERR "$what $id cannot be deleted: ".$resp->status_line."\n";
+                return undef;
+            }
+        }
+    }
+    return 1;
+}
+
+# the delete workhorse; finds out which XML to upload to the API to
+# delete an object.
+#
+# Parameters:
+# see sub delete.
+#
+# Returns:
+# undef on error, else the new XML to send to the API.
+# The XML has to 
+# be wrapped in <osm>...</osm> or inside a <modify>...</modify>
+# in a changeset upload.
+
+sub determine_delete_action
+{
+    my ($what, $id, $changeset, $recursive, $indent) = @_;
+
+    my $copy=0;
+    my $out = "";
+    my $members = [];
+    my $version;
+    my $user;
+
+    my $resp = OsmApi::get("$what/$id");
+    if (!$resp->is_success)
+    {
+        print STDERR " "x$indent;
+        print STDERR "$what $id cannot be retrieved: ".$resp->status_line."\n";
+        return undef;
+    }
+
+    my $c = $resp->content();
+
+    foreach (split(/\n/, $resp->content()))
+    { 
+        if (/<$what/) 
+        { 
+            /\sid="([^"]+)"/ or die; 
+            die unless $id eq $1; 
+            /\sversion="([^"]+)"/ or die; 
+            $version = $1;
+            /user="([^"]+)/;
+            $user=$1;
+            $copy = 1;
+            $out = $_;
+            $out =~ s/">/"\/>/g;
+            $members = [];
+        } 
+        elsif ($copy) 
+        { 
+            $copy=0 if (/<\/$what/);
+            if (/<nd ref=.(\d+)/)
+            {
+                push(@$members, { type => "node", id => $1 });
+            }
+            elsif (/<member.*type=.(way|node|relation).*id=.(\d+)/)
+            {
+                push(@$members, { type => $1, id => $2 });
+            }
+        } 
+    }; 
+
+    print STDERR " "x$indent;
+    print STDERR "$what $id last modified by $user (version $version) - deleting\n",
+    $out =~ s/changeset="\d+"/changeset="$changeset"/;
+    if ($recursive && scalar(@$members))
+    {
+        print STDERR " "x$indent;
+        print STDERR "recursively deleting members of $what $id\n";
+        foreach (@$members)
+        {
+            if (!defined($globalListOfDeletedStuff->{$_->{type}.$_->{id}}))
+            {
+                my $ua = determine_delete_action($_->{type}, $_->{id}, $changeset, 1, $indent + 2);
+                $out = $out . $ua if defined($ua);
+                $globalListOfDeletedStuff->{$_->{type}.$_->{id}} = 1;
+            }
+        }
+    }
+    return $out;
+}
+
+1;
---
+++ OsmApi.pm
@@ -0,0 +1,161 @@
+#!/usr/bin/perl
+
+# OsmApi.pm
+# ---------
+#
+# Implements OSM API connectivity
+#
+# Part of the "osmtools" suite of programs
+# Originally written by Frederik Ramm <frederik@remote.org>; public domain
+
+package OsmApi;
+
+use strict;
+use warnings;
+use LWP::UserAgent;
+use MIME::Base64;
+
+our $prefs;
+our $ua;
+our $dummy;
+
+BEGIN
+{
+
+    $prefs = { "dryrun" => 1 };
+
+    open (PREFS, $ENV{HOME}."/.osmtoolsrc") or die "cannot open ". $ENV{HOME}."/.osmtoolsrc";
+    while(<PREFS>)
+    {
+        if (/^(\S+)\s*=\s*(.*)/)
+        {
+            $prefs->{$1} = $2;
+        }
+    }
+    close (PREFS);
+
+    foreach my $required("username","password","apiurl")
+    {
+        die $ENV{HOME}."/.osmtoolsrc does not have $required" unless defined($prefs->{$required});
+    }
+
+    if (!defined($prefs->{instance}))
+    {
+        $prefs->{instance} = sprintf "%010x", $$ * rand(100000000);
+        open(PREFS, ">>".$ENV{HOME}."/.osmtoolsrc");
+        printf PREFS "instance=".$prefs->{instance};
+        close(PREFS);
+    }
+
+    $prefs->{apiurl} =~ m!https?://([^/]+)/!;
+    my $host = $1;
+    $host .= ":80" unless ($host =~ /:/);
+    $ua = LWP::UserAgent->new;
+    $ua->credentials($host, "Web Password", $prefs->{username}, $prefs->{password});
+    my $revision = '$Revision: 30253 $';
+    my $revno = 0;
+    $revno = $1 if ($revision =~ /:\s*(\d+)/);
+    $ua->agent("osmtools/$revno ($^O, ".$prefs->{instance}.")");
+    $ua->timeout(600);
+
+    $prefs->{debug} = $prefs->{dryrun} unless (defined($prefs->{debug}));
+
+    $dummy = HTTP::Response->new(200);
+}
+
+sub repeat
+{
+    my $req = shift;
+    my $resp;
+    for (my $i=0; $i<3; $i++)
+    {
+        $resp = $ua->request($req);
+        return $resp unless ($resp->code == 502 || $resp->code == 500);
+        sleep 1;
+    }
+    return $resp;
+}
+
+sub get
+{
+    my $url = shift;
+    my $req = HTTP::Request->new(GET => $prefs->{apiurl}.$url);
+    my $resp = repeat($req);
+    debuglog($req, $resp) if ($prefs->{"debug"});
+    return($resp);
+}
+
+sub get_with_credentials
+{
+    my $url = shift;
+    my $req = HTTP::Request->new(GET => $prefs->{apiurl}.$url);
+    $req->header("Authorization" => "Basic ".encode_base64($prefs->{username}.":".$prefs->{password}));
+    my $resp = repeat($req);
+    debuglog($req, $resp) if ($prefs->{"debug"});
+    return($resp);
+}
+
+sub put
+{
+    my $url = shift;
+    my $body = shift;
+    return dummylog("PUT", $url, $body) if ($prefs->{dryrun});
+    my $req = HTTP::Request->new(PUT => $prefs->{apiurl}.$url);
+    $req->header("Content-type" => "text/xml");
+    $req->content($body) if defined($body);
+    my $resp = repeat($req);
+    debuglog($req, $resp) if ($prefs->{"debug"});
+    return $resp;
+}
+
+sub post
+{
+    my $url = shift;
+    my $body = shift;
+    return dummylog("POST", $url, $body) if ($prefs->{dryrun});
+    my $req = HTTP::Request->new(POST => $prefs->{apiurl}.$url);
+    $req->content($body) if defined($body);
+    $req->header("Content-type" => "text/xml");
+    my $resp = repeat($req);
+    debuglog($req, $resp) if ($prefs->{"debug"});
+    return $resp;
+}
+
+sub delete
+{
+    my $url = shift;
+    my $body = shift;
+    return dummylog("DELETE", $url, $body) if ($prefs->{dryrun});
+    my $req = HTTP::Request->new(DELETE => $prefs->{apiurl}.$url);
+    $req->header("Content-type" => "text/xml");
+    $req->content($body) if defined($body);
+    my $resp = repeat($req);
+    debuglog($req, $resp) if ($prefs->{"debug"});
+    return $resp;
+}
+
+sub debuglog
+{
+    my ($request, $response) = @_;
+    printf STDERR "%s %s... %s %s (%db)\n",
+        $request->method(), 
+        $request->uri(), 
+        $response->code(), 
+        $response->message(), 
+        length($response->content());
+}
+
+sub dummylog
+{
+    my ($method, $url, $body) = @_;
+    print STDERR "$method $url\n";
+    print STDERR "$body\n\n";
+    return $dummy;
+}
+sub set_timeout
+{
+    my $to = shift;
+    $ua->timeout($to);
+}
+
+1;
